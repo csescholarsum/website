e24ad23e044a7f2777a7489e51398eafcb537128
Jeff Sallans <jefsallans@yahoo.com>
2012-09-11 16:25:59 -0400

    member home update4

--- public_html/index.php
+++ public_html/index.php
@@ -105,12 +105,9 @@ PURPOSE: website main page
 
 <?php
 
-		if (!isset($_SESSION['type'])) {
 
-		}
-		else {
-			echo "Welcome, " . $_SESSION['account_type'] . "\n";
-		}
+		echo "Welcome, " . $_SESSION['account_type'] . "\n";
+
 
 ?>			
 			<a href="https://web.eecs.umich.edu/~cseschol/login/index.php">
