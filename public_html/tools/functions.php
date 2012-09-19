ce9a698afdfe28342dedb1407b451534aeedcd29
Jeff Sallans <jefsallans@yahoo.com>
2012-09-19 15:43:14 -0400

    another fix

--- public_html/tools/functions.php
+++ public_html/tools/functions.php
@@ -62,7 +62,7 @@ function AddResume($temp_files, $uniqname, $save_path) {
 		}
 		else {
 
-			return var_dump($_FILES['resumeFile']);
+			return "" . var_dump($_FILES['resumeFile']);
 			//return "File didn't upload.";
 		}
 	}
