b757650655375604168541911aaffee042b28bf2
Jeff Sallans <jefsallans@yahoo.com>
2012-09-20 16:25:51 -0400

    resource update

--- public_html/content/resources.php
+++ public_html/content/resources.php
@@ -1,37 +1,56 @@
 <p>
-	Tutors Sign Up Here:
-	<br />
-  	&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="https://docs.google.com/spreadsheet/viewform?fromEmail=true&formkey=dGotdW9VR0U0eDNaV25xZnVnZkFiLXc6MQ">Sign Up</a><br />
+  Brightmoor Service Day:
+  <br />
+  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_BLANK" href="https://docs.google.com/spreadsheet/viewform?fromEmail=true&formkey=dF9nWVNHbjJEQVpMaXk0dmtrbF8xeWc6MQ">
+    Sign up here
+  </a>
+  <br />
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;You must sign up if you plan on attending! Transportation will be provided and therefore we need a headcount to plan for.
 </p>
 
 <p>
-	Slice of Life (Sept 27th, Oct 3rd, 8th, 25th):
-	<br />
-  	&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="https://docs.google.com/spreadsheet/viewform?formkey=dHNmNUR3WElJZDk2aElLZHpsZlMtdkE6MQ#gid=0">Sign Up</a><br />
+  Orbitz (Sept 23rd):
+  <br />
+  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_BLANK" href="https://docs.google.com/spreadsheet/viewform?fromEmail=true&formkey=dDZFRzlNdWlCaHMtVEJxZ2hHRFdPZmc6MQ" >
+    Sign up here
+  </a>
+  <br />
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sign up NOW! Only the first 20 people will be able to attend!
 </p>
 
 <p>
-	Orbitz Whirleyball Sign Up (Sept 23rd):
-	<br />
-  	&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="https://docs.google.com/spreadsheet/viewform?fromEmail=true&formkey=dDZFRzlNdWlCaHMtVEJxZ2hHRFdPZmc6MQ">Sign Up</a><br />
+  Slice of Life (Sept 27th, Oct 3rd, 8th, 25th):
+  <br />
+  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_BLANK" href="https://docs.google.com/spreadsheet/viewform?formkey=dHNmNUR3WElJZDk2aElLZHpsZlMtdkE6MQ#gid=0" >
+    Sign up here
+  </a>
+  <br />
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If you participate in Slice of Life, please email Kristen Dwan at dwankri@umich.edu with details of what day and how many hours you are participating. The info session also counts for community service.
+</p>
+
+<p>
+  Tutoring:
+  <br />
+  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_BLANK" href="https://docs.google.com/spreadsheet/viewform?fromEmail=true&formkey=dGotdW9VR0U0eDNaV25xZnVnZkFiLXc6MQ" >
+    Sign up here
+  </a>
+  <br />
+</p>
+
+<p>
+  Tech Day (Sept 28th, 29th):
+  <br />
+  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_BLANK" href="http://techday.engin.umich.edu/volunteer/register" >
+    Sign up here
+  </a>
+  <br />
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;If you volunteer at tech day please send an email to Kristen Dwan at dwankri@umich.edu and let her know what hours you will be working the event so they can be counted for your community service. 
 </p>
 
 <p>
 	Our New Constitution (Sept 8th):
 	<br />
   	&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="https://docs.google.com/a/umich.edu/file/d/0B9fVa_kuwgSYdGpFNEdiUWZTd00/preview">View Our Constitution</a><br />
-</p>
-
-
-<p>
-  Relay For Life Sign Up Sheet (Febuary 20th):<br />
-  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="https://docs.google.com/spreadsheet/ccc?key=0AtfVa_kuwgSYdGgyVEt6SHVYQ3IzcXViVXM1S0tIYnc">Sign Up Google Doc</a><br />
-</p>
-
-
-<p>
-  Winter 12 Mass Meeting Info (January 12th):<br />
-  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="resources/mass_meetingW12/Winter12_mass_meeting.pdf">Mass Meeting Info</a><br />
 </p>
 
 <p>
