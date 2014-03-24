084c64eb559af961f5ad8d12b01d5560c35e3a9e
Kris McAtee <kmcatee@umich.edu>
2014-03-24 13:17:53 -0400

    resources update

--- public_html/bottom.php
+++ public_html/bottom.php
@@ -2,7 +2,7 @@
 
 			<div id="footer"> 
 				<p class="legal">	<a href="index.php?slide_page=about">About Us</a> | 
-				<a href="index.php?slide_page=officers">Contact Us</a> |Copyright &copy; 2012 CSE scholars. All rights reserved.</p>
+				<a href="index.php?slide_page=officers">Contact Us</a> |Copyright &copy; 2014 CSE scholars. All rights reserved.</p>
 				<p class="credit"><a href="http://www.umich.edu/">University of Michigan</a></p>
 			</div>
 		<br />
--- public_html/content/resources.php
+++ public_html/content/resources.php
@@ -1,7 +1,7 @@
 <p>
   Tutoring:
   <br />
-  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_BLANK" href="https://docs.google.com/spreadsheet/viewform?fromEmail=true&formkey=dGotdW9VR0U0eDNaV25xZnVnZkFiLXc6MQ" >
+  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_BLANK" href=" https://docs.google.com/spreadsheet/viewform?fromEmail=true&formkey=dGotdW9VR0U0eDNaV25xZnVnZkFiLXc6MQ" >
     Sign up here
   </a>
   <br />
@@ -12,23 +12,12 @@
 	<br />
   	&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="https://docs.google.com/a/umich.edu/file/d/0B9fVa_kuwgSYdGpFNEdiUWZTd00/preview">View Our Constitution</a><br />
 </p>
-
 <p>
-	Intel's Resume and Career Fair Tips:<br />
-	&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="resources/Intel/intel_slides.pptx">Resume & Career Fair Tips Fall 2013</a><br />
-</p>
-
-<p>
-	Brightmoor Signup Form for Fall 2013:<br />
-	&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="https://docs.google.com/forms/d/1-b5oJYfwhisvecd8jxC198-zBp1N_8ftmDv4F-i6wTw/viewform">Google Form</a><br />
-</p>
-
-<p>
-  Resume Tips from Glenn Galler at IBM (Novemeber 18th):<br />
-  &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href="resources/IBM/Univ%20of%20Michigan%20CS%20Recruiting%20Advice%20Nov%202010.pdf">Resume Tips</a><br />
+	Brightmoor Signup Form for Winter 2014:<br />
+	&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;<a target="_blank" href=" https://docs.google.com/a/umich.edu/spreadsheet/ccc?key=0ApLtaXDShdpedER5STFhSnM1V2VPd2RhM0Y4XzZDOWc&usp=gmail">Google Form</a><br />
 </p>
 
 <p>
 CS Bootcamp Signup<br />
-<a target="_blank" href="https://docs.google.com/forms/d/1Z-0LcAP6pApjIXZblaBddNKTvcCt2LvWTqC1mydktTI">Sign up here</a><br />
+<a target="_blank" href="https://docs.google.com/a/umich.edu/forms/d/1KUjOgUivYDEcFN8ohVAAzhpJjepglk0N3VsRdACi0dg/viewform">Sign up here</a><br />
 </p>
