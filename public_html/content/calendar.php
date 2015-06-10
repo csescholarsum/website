68b1bbe0cb608187b4d92ff9c92193612c4a8997
CSE Scholars Acct <cseschol@web.eecs.umich.edu>
2015-06-09 21:22:10 -0400

    website is up now, just need to update it

--- public_html/content/calendar.php
+++
@@ -1,7 +0,0 @@
-<iframe src="https://www.google.com/calendar/embed?src=csescholarsum%40gmail.com&ctz=America/New_York" 
-	   style=" border-width:1 " 
-	   width="650"
-	   frameborder="0" 
-	   height="610"
-	   marginwidth="400">
-</iframe>
