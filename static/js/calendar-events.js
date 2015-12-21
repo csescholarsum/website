d9d7b64fa10156d8fb742da2947e8cf2c6ba7d66
Taku <torus@umich.edu>
2015-12-21 15:36:25 -0500

    pushing old changes

--- static/js/calendar-events.js
+++ static/js/calendar-events.js
@@ -5,18 +5,32 @@ var update_calendar = function() {
   var year = d.getUTCFullYear();
   var month = d.getUTCMonth() + 1;
   var day = d.getUTCDate();
-  var dateString = year+"-"month+"-"+day+"T0:00:00Z"
-  var cal_URL = "https://www.googleapis.com/calendar/v3/calendars/csescholarsum@gmail.com/events?maxResults=15&timeMin="+dateString+"&key=AIzaSyAMdAKe1QdTiRUpW8oLZAfkLTGzz3NhUfM"
+  var dateString = year + "-" + month+"-"+day+"T0:00:00Z";
+  var cal_URL = "https://www.googleapis.com/calendar/v3/calendars/csescholarsum@gmail.com/events?maxResults=15&timeMin="+dateString+"&key=AIzaSyAMdAKe1QdTiRUpW8oLZAfkLTGzz3NhUfM";
 
 
   $.ajax({
      type: "GET",
      dataType: "json",
      url: cal_URL,
-     data: getOptions,
+    //  data: getOptions,
      cache: false,
      success: function (data,textStatus) {
-        console.log(data);
+        for(var i = 0; i < data["items"].length; i++){
+          var to_add = document.createElement("div");
+          var title = document.createElement("h3");
+
+          title.innerHTML =  data["items"][i]["summary"] ;
+          to_add.appendChild(title);
+
+          var datetime = document.createElement("h3");
+          var location = document.createElement("h3");
+          var desc = document.createElement("p");
+
+          console.log($('#cal'));
+          $('#cal').append(to_add);
+
+        }
      },
      error: function(e,f,g) {}
   });
@@ -24,4 +38,4 @@ var update_calendar = function() {
 
 };
 
-update_calendar()
+update_calendar();
