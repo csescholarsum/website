d989d048f508e87486857800a730c42bef877779
Taku <torus@umich.edu>
2016-02-13 15:47:03 -0500

    database stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
-from flask.ext.mobility import Mobility
 
 app = Flask(__name__, template_folder="views")
-Mobility(app)
-app.secret_key = 'YELLOW SUBMARINE'
 
 from app.controllers.database import mysql
 mysql.init_app(app)
